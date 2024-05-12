3193abae4f440ac09493e5221d629a1fc9fd35b6
grenov24 <vaclav.greno@gmail.com>
2024-05-12 14:40:42 +0200

    předělání html pro header, vytvoření úprav v js

--- html_sablony/script.js
+++ html_sablony/script.js
@@ -1,6 +1,32 @@
-const adminNavigace = document.getElementsByClassName('admin_nav');
-const closeAdminNavigace = document.getElementsByClassName('close');
+//const adminNav = document.querySelector('nav')
+//console.log(adminNav);
 
-closeAdminNavigace.addEventListener('click', () => {
-    odkazyNavigace.classList.remove('admin_nav');  
-});
+//adminNav.remove();
+
+
+
+
+
+
+
+//nahrazení "přihlásit se" za iconu při zmenšení stránky na rozlišení 1001px
+const headerUserInterface = document.getElementsByClassName('header_logo_h')[0];
+
+function createLoginIcon(linkUrl, imgUrl) {
+    
+    const linkIcon = document.createElement('a');
+    linkIcon.setAttribute('href', linkUrl);
+
+    const imgIcon = document.createElement('img');
+    imgIcon.setAttribute('src', imgUrl);
+
+    linkIcon.appendChild(imgIcon);
+    linkIcon.classList.add('login_icon');
+    
+
+    return linkIcon;
+}
+
+headerUserInterface.appendChild(
+    createLoginIcon("admin_login.html","user.svg")
+);
