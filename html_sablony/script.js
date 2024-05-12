4bbde02250246aabf8516af8126eb9d5650beacf
grenov24 <vaclav.greno@gmail.com>
2024-05-12 16:55:12 +0200

    přidání popisků k scrip.js

--- html_sablony/script.js
+++ html_sablony/script.js
@@ -1,51 +1,52 @@
-//const adminNav = document.querySelector('nav')
-//console.log(adminNav);
-
-//adminNav.remove();
-
-
-
+//js pro nahradu textu "přihlásit se" za ikonu user.svg
+//pokud je obrazovka menší než 1001px
 const headerUserInterface = document.getElementsByClassName('header_logo_h')[0];
 const loginText = document.getElementsByClassName('login')[0];
 
-
+//vytvořit ikonu na přihlašení - nastavení cesty k obrázku a kam ikona povede
 function createImageLink(linkUrl, imgUrl) {
-    const linkElement = document.createElement('a');
-    linkElement.setAttribute('href', linkUrl);
+    const linkIcon = document.createElement('a');
+    linkIcon.setAttribute('href', linkUrl);
   
     
-    const imgElement = document.createElement('img');
+    const imgIcon = document.createElement('img');
   
     
-    imgElement.setAttribute('src', imgUrl);
+    imgIcon.setAttribute('src', imgUrl);
   
-    linkElement.appendChild(imgElement);
+    linkIcon.appendChild(imgIcon);
   
-    linkElement.classList.add('login_icon');
-    return linkElement;
+    linkIcon.classList.add('login_icon');
+    return linkIcon;
   }
+//vybrání podle responzivity kdy ikonu zobrazit
+const mediaQueryMax = window.matchMedia('(max-width: 1001px)');
 
-const mediaQueryMax = window.matchMedia('(max-width: 1000px)');
-var linkElement;
+var linkIcon;
+
+// funkce zkontroluje jestli ikona již existuje
+// pokud okno je menší 1001px zkontroluje jestli ikona existuje
+// pokud ikona neexistuje, smaže <a> z html a nahradí ho ikonou
+// pokud okno je opět zvětšeno nad 1001px ikona se smaže a <a> z html se znovu vytvoří
 function changeToIcon() {
 if (mediaQueryMax.matches) {
-    if (!linkElement) {
-    linkElement = createImageLink('https://example.com', 'user.svg');
-    headerUserInterface.appendChild(linkElement);
+    if (!linkIcon) {
+    linkIcon = createImageLink('admin_login.html', 'user.svg');
+    headerUserInterface.appendChild(linkIcon);
     loginText.remove()
     }
 }
 else {
-    if (linkElement) {
-    linkElement.remove();
-    linkElement = null;
+    if (linkIcon) {
+    linkIcon.remove();
+    linkIcon = null;
     headerUserInterface.appendChild(loginText);
     }
 }   
 }
 changeToIcon();
 
-
+//vytvoří posluchač udalosti na velikost okna -- proč je přeškrtnutý??
 mediaQueryMax.addListener(changeToIcon);
 
   
