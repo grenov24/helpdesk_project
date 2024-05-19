3635988a8f2467f317001b15bd30dfaac9bcb271
Václav Greňo <vaclav.greno@gmail.com>
2024-05-19 19:17:50 +0200

    nalinkovani controlleru se šablonou admin

--- symfony/src/Controller/DashboardController.php
+++ symfony/src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RequestRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/prehled', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(RequestRepository $requestRepository): Response
     {
+        $requests = $requestRepository->findBy([], ['id' => 'DESC'], 8);
+
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'requests' => $requests,
         ]);
     }
 }
